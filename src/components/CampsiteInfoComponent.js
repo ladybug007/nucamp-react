a53b2a5391d18d82ac45621d796e3db956f134ad
Priya <49821685+ladybug007@users.noreply.github.com>
2020-05-30 17:39:50 -0500

    Presentational and container components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -33,9 +33,11 @@ class CampsiteInfo extends Component {
     render(campsite) {
         if (this.props.campsite) {
             return (
+                <div className='container'>
                 <div className='row'>
                     {this.renderCampsite(this.props.campsite)}
                     {this.renderComments(this.props.campsite.comments)}
+                </div>
                 </div>
             );
         }
