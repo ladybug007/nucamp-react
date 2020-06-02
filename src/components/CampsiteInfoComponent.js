c00081da3e024963584b23429aa1d2166b254ff8
Priya <49821685+ladybug007@users.noreply.github.com>
2020-06-01 23:46:30 -0500

    Breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 
     
@@ -34,6 +35,14 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         if (props.campsite) {
             return (
                 <div className='container'>
+                    <div className ='row'>
+                <div className = 'col'>
+                    <Breadcrumb>
+                    <BreadcrumbItem><Link to='/directory'>Directory</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+                </div>
                 <div className='row'>
                     <RenderCampsite campsite = {props.campsite}/>
                     <RenderComments comments ={props.comments}/>
