4200b75745144b421cb163b071f7bbcb306e0a26
Priya <49821685+ladybug007@users.noreply.github.com>
2020-06-19 18:44:01 -0500

    React-animations

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -12,6 +13,11 @@ const minLength = len => val => val && val.length >= len;
 function RenderCampsite({ campsite }) {
     return (
         <div className='col-md-5 m-1'>
+            <FadeTransform
+              in  
+              transformProps = {{
+                  exitTransform: 'scale(0.5) translateY(-50%)'
+              }}>
             <Card>
                 <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
@@ -19,6 +25,7 @@ function RenderCampsite({ campsite }) {
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         </div>
     );
 }
@@ -26,13 +33,23 @@ function RenderCampsite({ campsite }) {
 function RenderComments({ comments, postComment,campsiteId  }) {
     if ({ comments }) {
         return (
-            <div>
+            
                 <div className='col-md-5 m-1'>
                     <h4>Comments</h4>
-                    {comments.map(comment => <div>{comment.text}<br />
-                        -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</div>)}
-
-                </div>
+                    <Stagger in>
+                    {comments.map(comment => {
+                        return(
+                            <Fade in key={comment.id}>
+                                <div>
+                                    <p>{comment.text}<br />
+                        -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                        </p>
+                        </div>
+                        </Fade>);
+                        })}
+                    
+                
+                </Stagger>
                 <CommentForm campsiteId={campsiteId} postComment={postComment}/>
             </div>
 
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardBody, CardText, CardTitle, CardImg } from 'reactstrap';
 import{Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform} from 'react-animation-components';
 
 
 
@@ -17,14 +18,19 @@ function RenderCard({item, isLoading, errMess}) {
         );
     }
     return (
-        <Card>
 
+        <FadeTransform
+          in 
+          transformProps = {{exitTransform: 'scale(0.5) translateY(50%)'
+          }}>
+        <Card>
             <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
+        </FadeTransform>
 
     );
 }
